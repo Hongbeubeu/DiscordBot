4ea3e35cc0e4c1e96987a87f565a21f6a3b01263
hongnv <hongnv@iecgames.net>
2022-04-22 18:15:58 +0700

    update

--- TimeKeeper.py
+++ TimeKeeper.py
@@ -1,9 +1,10 @@
+from cmath import log
 from http import client
 from discord_slash import SlashCommand, SlashContext
 from discord.ext import commands
 from discord_slash.utils.manage_commands import create_choice, create_option
 import pygsheets
-import datetime
+from datetime import datetime
 
 SHEET_KEY = "1Inz2N5Oy5wxoBK0NI173qfPeQwAU2OtcORR5lf67uOg"
 DISCORD_BOT_KEY = "OTY0ODEwNDEzNTk2MzY0ODIw.YlqDtw.GF9RXBe7BbGeIB523GiSBbOPTm8"
@@ -61,15 +62,15 @@ async def xin_nghi(ctx: SlashContext, date: str, part_of_day: str, reason: str):
     if(ctx.channel.id != ANNOUNCEMENTS_ID):
         await ctx.send("Sang channel announcements để xin nghỉ")
         return
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     current_year = current_date.year
     try:
-        input_date = datetime.datetime.strptime(date, '%d/%m')
+        input_date = datetime.strptime(date, '%d/%m')
         if(current_date.month == 12 and input_date.month == 1):
             current_year += 1
-            input_date = input_date.replace(year=current_year)
+        input_date = input_date.replace(year=current_year)
     except ValueError:
-        await ctx.send("Lỗi nhập sai ngày: Nhập ngày với định dạng d/m")
+        await ctx.send("Lỗi nhập sai ngày: Nhập ngày với định dạng ngày/tháng")
         return
 
     if((current_date - input_date).days > 0):
@@ -81,8 +82,10 @@ async def xin_nghi(ctx: SlashContext, date: str, part_of_day: str, reason: str):
         wks = sh.worksheet_by_title(
             "{}/{}".format(input_date.month, input_date.year))
         row = wks.find("{}".format(ctx.author.id))[0].row
-        col = wks.find(
-            "{}/{}/{}".format(input_date.month, input_date.day, input_date.year))[0].col
+        print("{}/{}/{}".format(input_date.month, input_date.day, input_date.year))
+        col = wks.find("{}/{}/{}".format(input_date.month,
+                       input_date.day, input_date.year))[0].col
+        print("row {}, col {}".format(row, col))
         value = "nghỉ "
         if (part_of_day == 'sáng' or part_of_day == 'chiều'):
             value += "1/2"
@@ -160,14 +163,28 @@ async def xin_nghi_nhieu_ngay(ctx: SlashContext, from_part_of_day: str, from_dat
     if(ctx.channel.id != ANNOUNCEMENTS_ID):
         await ctx.send("Sang channel announcements để xin nghỉ")
         return
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     current_year = current_date.year
     try:
-        from_date = datetime.datetime.strptime(from_date, "%d/%m")
-        to_date = datetime.datetime.strptime(to_date, "%d/%m")
-        if(current_date.month == 12 and input_date.month == 1):
-            current_year += 1
-        input_date = input_date.replace(year=current_year)
+        if(from_date.count(from_date) <= 5):
+            from_date = datetime.strptime(from_date, "%d/%m")
+            to_date = datetime.strptime(to_date, "%d/%m")
+            if(current_date.month == 12 and from_date.month != 12):
+                next_year = current_year + 1
+                from_date = from_date.replace(year=next_year)
+                to_date = to_date.replace(year=next_year)
+            elif(current_date.month == 12 and to_date.month != 12):
+                next_year = current_year + 1
+                from_date = from_date.replace(year=current_year)
+                to_date = to_date.replace(year=next_year)
+            else:
+                from_date = from_date.replace(year=current_year)
+                to_date = to_date.replace(year=current_year)
+        else:
+            from_date = datetime.strptime(from_date, "%d/%m/%y")
+            to_date = datetime.strptime(to_date, "%d/%m/%y")
+        print("{}->{}".format(from_date, to_date))
+
     except ValueError:
         await ctx.send("Lỗi fomat ngày hoặc ngày trong quá khứ")
         return
@@ -176,5 +193,30 @@ async def xin_nghi_nhieu_ngay(ctx: SlashContext, from_part_of_day: str, from_dat
         await ctx.send("Lỗi nhập sai ngày bắt đầu hoặc kết thúc {}".format(ctx.author.id))
         return
 
-    await ctx.send("Xinnnnnn {}".format(ctx.author.id))
+    try:
+        await ctx.send("{} Xin nghỉ từ {} ngày: {} tới {} ngày: {} với lý do: {}".format(ctx.author.name, from_part_of_day, from_date.strftime('%d/%m'), to_part_of_day, to_date.strftime('%d/%m'), reason))
+        sh = gc.open_by_key(SHEET_KEY)
+        wks = sh.worksheet_by_title(
+            "{}/{}".format(from_date.month, from_date.year))
+        row = wks.find("{}".format(ctx.author.id))[0].row
+        col = wks.find("{}/{}/{}".format(from_date.month,
+                       from_date.day, from_date.year))[0].col
+        value = "nghỉ"
+        if(from_part_of_day == "chiều"):
+            value += " 1/2"
+        wks.update_value((row, col), value)
+        count_day = (to_date - from_date).days
+        if(count_day > 1):
+            for i in range(1, count_day):
+                col += 1
+                value = "nghỉ"
+                wks.update_value((row, col), value)
+        col += 1
+        value = "nghỉ"
+        if(to_part_of_day == "sáng"):
+            value += " 1/2"
+        wks.update_value((row, col), value)
+
+    except ValueError:
+        await ctx.send("Lỗi rồi! Hãy xin nghỉ lại")
 client.run(DISCORD_BOT_KEY)
